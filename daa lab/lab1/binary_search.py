f9fcc3d2b0f210e092060c21b6d3f74f24370022
Sameer9969 <thakursameer.singhrajput@gmail.com>
2026-07-23 22:55:39 +0530

    lab

---
+++ daa lab/lab1/binary_search.py
@@ -0,0 +1,31 @@
+def binary_search(arr, low, high, target):
+
+    # Base Case
+    if low > high:
+        return -1
+
+    # Middle nikalna
+    mid = (low + high) // 2
+
+    # Agar element mil gaya
+    if arr[mid] == target:
+        return mid
+
+    # Left side me search karo
+    if target < arr[mid]:
+        return binary_search(arr, low, mid - 1, target)
+
+    # Right side me search karo
+    return binary_search(arr, mid + 1, high, target)
+
+
+# Main Program
+arr = [2, 5, 8, 12, 16, 23, 38, 56, 72, 91]
+target = 23
+
+result = binary_search(arr, 0, len(arr) - 1, target)
+
+if result == -1:
+    print("Element not found")
+else:
+    print("Element found at index", result)
